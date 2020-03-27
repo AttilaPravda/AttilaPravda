a0086711d455b796640123d4c7a9e852aa5418ca
AttilaPravda <attilabre@gmail.com>
2020-03-27 14:27:32 +0100

    Updates

---
+++ precache-manifest.87d110d7acae98123a7e234e216f0b98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1faa21b2b87173dc934bc3fd24c6b6",
+    "url": "/CMG/index.html"
+  },
+  {
+    "revision": "d96ca691445c45d2ebfd",
+    "url": "/CMG/static/css/2.c077b818.chunk.css"
+  },
+  {
+    "revision": "5858952e19ddbb291c28",
+    "url": "/CMG/static/css/main.42af6d4a.chunk.css"
+  },
+  {
+    "revision": "d96ca691445c45d2ebfd",
+    "url": "/CMG/static/js/2.c62ad9e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CMG/static/js/2.c62ad9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5858952e19ddbb291c28",
+    "url": "/CMG/static/js/main.f7864e71.chunk.js"
+  },
+  {
+    "revision": "defde857306b4dcbed30",
+    "url": "/CMG/static/js/runtime-main.1db31ed1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/CMG/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/CMG/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/CMG/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/CMG/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ae23a172292167edccf4f5c1acbe60a3",
+    "url": "/CMG/static/media/slider-bg.ae23a172.jpg"
+  }
+]);
